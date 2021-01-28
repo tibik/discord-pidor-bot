f3b3695246a82e64de768157918d64f6e2103087
Misha Ovsepyan <m1shkaa666@gmail.com>
2021-01-28 16:46:23 +0300

    count formatter added

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -13,8 +13,15 @@ function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+// countFormatter(count, ['найдена', 'найдено', 'найдены']);
+function countFormatter(number, titles) {
+  const cases = [2, 0, 1, 1, 1, 2];
+  return titles[number % 100 > 4 && number % 100 < 20 ? 2 : cases[number % 10 < 5 ? number % 10 : 5]];
+}
+
 module.exports = {
   getRandomElement,
   asyncForEach,
   sleep,
+  countFormatter,
 };
